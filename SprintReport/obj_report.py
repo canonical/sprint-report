25c4c10f613c7f051e144905cb6a65f89f5aa308
Hon Ming Hui <honming.hui@canonical.com>
2023-10-09 18:45:24 +0100

    Add initial PoC of objective/epic report

---
+++ SprintReport/obj_report.py
@@ -0,0 +1,132 @@
+import argparse
+import re
+import sys
+
+from jira import JIRA, JIRAError
+from SprintReport.jira_api import jira_api
+
+jira_server = ""
+
+def get_bug_id(summary):
+    "Extract the bug id from a jira title which would include LP#"
+    id = ""
+
+    if "LP#" in summary:
+        for char in summary[summary.find("LP#")+3:]:
+            if char.isdigit():
+                id = id + char
+            else:
+                break
+
+    return id
+
+def find_epic(jira_api, project):
+    if not jira_api or not project:
+        return {}
+
+    # Get JIRA issues in batch of 50
+    issue_index = 0
+    issue_batch = 50
+
+    found_issues = {}
+
+    while True:
+        start_index = issue_index * issue_batch
+        request = "project = \"{}\" AND status in (\"In Progress\", \"Done\") AND issueType = Epic ORDER BY status".format(project)
+        issues = jira_api.search_issues(request, startAt=start_index)
+
+        if not issues:
+            break
+
+        issue_index += 1
+
+        # For each issue in JIRA with LP# in the title
+        for issue in issues:
+            status = issue.fields.status
+            summary = issue.fields.summary
+            issue_type = issue.fields.issuetype.name
+            if hasattr(issue.fields, "parent"):
+                epic = jira_api.issue(issue.fields.parent.key)
+                epic_summary = epic.fields.summary
+            else:
+                epic_summary = "Other"
+            if found_issues.get(epic_summary) == None:
+                found_issues[epic_summary] = {}
+
+            found_issues[epic_summary][issue.key]= {
+                "key":issue.key,
+                "type":issue_type,
+                "epic":epic_summary,
+                "status":status,
+                "summary":summary}
+
+    return found_issues
+
+
+
+def key_to_md(key):
+    global jira_server
+    markdown_link = "[{}]({})"
+
+    return markdown_link.format(key, jira_server + "/browse/" + key)
+
+
+def insert_bug_link(text):
+    markdown_link = "[{}]({})"
+    bugid = get_bug_id(text)
+    bug= "LP#" + bugid
+    link = "https://pad.lv/" + bugid
+
+    return re.sub(bug, markdown_link.format(bug, link), text)
+
+
+def print_jira_issue(issue):
+    summary = issue["summary"]
+    status = issue["status"]
+    key = key_to_md(issue["key"])
+    if "LP#" in summary:
+        summary = insert_bug_link(summary)
+        print("   - {} - {}".format(summary, status))
+    else:
+        print("   - {} : {} - {}".format(key, summary, status))
+
+def print_jira_report(issues):
+    if not issues:
+        return
+
+    epic = ""
+
+    for issue in issues:
+        print(" - {}".format(issue))
+        for i in issues[issue]:
+            print_jira_issue(issues[issue][i])
+
+def main(args=None):
+    global jira_server
+    parser = argparse.ArgumentParser(
+        description=
+            "A script to return a a Markdown report of a Jira Project Objective/Epic"
+    )
+
+    parser.add_argument("project", type=str, help="key of the Jira project")
+
+    opts = parser.parse_args(args)
+
+    try:
+        api = jira_api()
+    except ValueError as e:
+        print(f"ERROR: Cannot initialize Jira API: {e}", file=sys.stderr)
+        sys.exit(1)
+
+    jira_server = api.server
+
+    jira = JIRA(api.server, basic_auth=(api.login, api.token))
+
+    print("# Project {}".format(opts.project))
+    # Search for JIRA Objective/Epic 
+    issues = find_epic(jira, opts.project)
+
+    print_jira_report(issues)
+
+# =============================================================================
+
